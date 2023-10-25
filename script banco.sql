eac099bfdfb7131d6cfd3aeb4174170b3f3101e8
Gabriel Carvalho <146982565+GabrielDSantosCarvalho@users.noreply.github.com>
2023-10-25 17:07:54 -0300

    Add files via upload

---
+++ script banco.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE saguadin;
+USE saguadin;
+
+--CRIAÇÃO DA TABELA DE USUARIOS
+
+CREATE TABLE usuarios(
+    usu_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    usu_login VARCHAR(20) NOT NULL,
+    usu_senha VARCHAR(50) NOT NULL,
+    usu_status CHAR(1),
+    usu_key VARCHAR(10)
+);
+
+--CRIAÇÃO DE TABELA CLIENTE
+
+CREATE TABLE cliente(
+    cli_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cli_nome VARCHAR(50) NOT NULL,
+    cli_email VARCHAR(100) NOT NULL,
+    cli_telefone BIGINT NOT NULL,
+    cli_cpf VARCHAR(20) NOT NULL,
+    cli_curso VARCHAR(50) NOT NULL,
+    cli_sala INT NOT NULL,
+    cli_status CHAR(1)
+);
